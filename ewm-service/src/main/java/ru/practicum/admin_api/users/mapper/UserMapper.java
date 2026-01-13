811e586c148fc4874346364cf1e66fc30888afe0
Dmitriy Prishchepa <dimaprishchepa@yandex.ru>
2026-01-13 22:01:03 +0300

    create postman collection for users requests

--- ewm-service/src/main/java/ru/practicum/admin_api/users/mapper/UserMapper.java
+++ ewm-service/src/main/java/ru/practicum/admin_api/users/mapper/UserMapper.java
@@ -2,11 +2,13 @@ package ru.practicum.admin_api.users.mapper;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
 import ru.practicum.admin_api.users.model.User;
 import ru.practicum.dtos.users.UserDto;
 
 import java.util.List;
 
+@Component
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserMapper {
     public UserDto mapToDto(User user) {
--- ewm-service/src/main/java/ru/practicum/exception/GlobalExceptionHandler.java
+++ ewm-service/src/main/java/ru/practicum/exception/GlobalExceptionHandler.java
@@ -2,12 +2,12 @@ package ru.practicum.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.practicum.exception.exceptions.ApiError;
 
-@ControllerAdvice
+@RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
